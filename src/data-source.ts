d851a68cc51cfb722047f2e2e2080c525e8f2a28
David Erenfeld <1020dudu@gmail.com>
2024-10-10 02:35:51 +0300

    Update deployment setup and compiled files

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,27 +4,17 @@ import { User } from "./entity/users_model";
 import { Trip } from "./entity/trips_model";
 import { Like } from "./entity/like_model";
 import { Comment } from "./entity/comment_model";
-import { readFileSync } from "fs";
 
 const connectDB = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER_NAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
   logging: false,
   synchronize: true,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
   entities: [User, Trip, Like, Comment],
-  extra: {
-    ssl: false,
-    // process.env.NODE_ENV === "production"
-    // ? {
-    //     rejectUnauthorized: true,
-    //     ca: readFileSync(process.env.SSL_CERT_PATH_POSTGRES).toString(),
-    //   }
-    // : false,
-  },
 });
 
 export default connectDB;
