be3afa8142d946b1c98957a6059bc702dbd9713b
David Erenfeld <1020dudu@gmail.com>
2024-11-02 22:45:52 +0200

    Update data-source configuration: conditional SSL loading based on environment for production and local development compatibility

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,13 +4,12 @@ import { User } from "./entity/users_model";
 import { Trip } from "./entity/trips_model";
 import { Like } from "./entity/like_model";
 import { Comment } from "./entity/comment_model";
-import { readFileSync } from "fs";
 
 const isProduction = process.env.NODE_ENV === "production";
 
 const connectDB = new DataSource({
   type: "postgres",
-  url: isProduction ? process.env.DATABASE_URL : undefined,
+  url: process.env.DATABASE_URL, // השתמש תמיד ב-DATABASE_URL בהרוקו
   host: isProduction ? undefined : process.env.DB_HOST,
   port: isProduction ? undefined : Number(process.env.DB_PORT),
   username: isProduction ? undefined : process.env.DB_USER_NAME,
@@ -21,13 +20,8 @@ const connectDB = new DataSource({
   entities: [User, Trip, Like, Comment],
   extra: {
     ssl: isProduction
-      ? {
-          rejectUnauthorized: true,
-          ca: process.env.SSL_CERT_PATH_POSTGRES
-            ? readFileSync(process.env.SSL_CERT_PATH_POSTGRES).toString()
-            : undefined,
-        }
-      : false,
+      ? { rejectUnauthorized: false } // השאר SSL פשוט ב-production
+      : false, // השבת SSL בסביבה מקומית
   },
 });
 
